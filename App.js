836cfc637405c0937cee42920ef24f5675582bd9
30lima <pedrolimaa354@gmail.com>
2025-06-02 01:50:10 -0300

    feat: add navigator

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import SplashScreen from './screens/SplashScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
