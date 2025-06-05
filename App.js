00aacb0e6b6ab5e52d7bcfb26ca4545f9f983444
30lima <pedrolimaa354@gmail.com>
2025-06-05 00:06:36 -0300

    feat: add drawer navigator

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import SplashScreen from './screens/SplashScreen';
 import MainTabs from './navigator/MainTabs';
+import DrawerNavigator from './navigator/DrawerNavigator';
 
 
 const Stack = createNativeStackNavigator();
@@ -15,7 +16,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="MainTabs" component={MainTabs} />
+        <Stack.Screen name="Main" component={DrawerNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
